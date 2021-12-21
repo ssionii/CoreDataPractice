7d85530f536098b02df0c0cd6c05e469392b29f2
ssionii <ssionii353@gmail.com>
2021-12-21 17:16:52 +0900

    Initial Commit

---
+++ CoreDataPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataPractice
+//
+//  Created by Yang Siyeon on 2021/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
